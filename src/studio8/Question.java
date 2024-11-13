a5a501297c468f178787aa15d80470338f6e237d
kiraul <kiraul@kiras-air.dhcp.wustl.edu>
2024-11-13 13:46:28 -0600

    here

--- src/studio8/Question.java
+++ src/studio8/Question.java
@@ -34,7 +34,8 @@ public class Question {
 	public static void main(String[] args) {
 		Question q = new Question("What number studio is this?", "8", 2);
 		q.displayPrompt();
-		System.out.println(q.checkAnswer("hi"));
 		System.out.println(q.checkAnswer("8"));
-	}
+		Question r = new Question("What number assignment was due today?", "7", 2);
+		r.displayPrompt();
+		System.out.println(r.checkAnswer("7")); }
 }
